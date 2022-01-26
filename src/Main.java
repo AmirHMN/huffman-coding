dd8860a72d95521e3da8b84ad116bb1882ff6cba
AmirHmn <amir.hosein6979@gmail.com>
2022-01-26 11:54:27 +0330

    modified main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Huffman.encoding("abacabcabcdaabcde"));
+        String result = init();
+        System.out.println(result);
+    }
 
+    public static String init() {
+        Scanner input = new Scanner(System.in);
+        switch (input.nextInt()) {
+            case 1:
+                String text = input.nextLine();
+                return Huffman.encoding(text);
+            case 2:
+                Queue queue = new Queue();
+                int numbers = input.nextInt();
+
+                for (int i = 0; i < numbers; i++) {
+                    queue.enqueue(new Tree(input.next(), input.nextInt()));
+                    input.nextLine();
+                }
+                String encoded = input.next();
+                return Huffman.decoding(queue, encoded);
+            default:
+                break;
+        }
+        return "";
     }
 }
