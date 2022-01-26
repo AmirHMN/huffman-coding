f29c4652e98b3a8d39806fc31afcf7a7eba35966
AmirHmn <amir.hosein6979@gmail.com>
2022-01-26 11:53:33 +0330

    add decoding function

--- src/Huffman.java
+++ src/Huffman.java
@@ -1,13 +1,16 @@
 import java.util.HashMap;
 
 public class Huffman {
+    static boolean isEncoding;
     static String text;
+    static String encoded;
     static Queue queue;
     static Tree huffmanTree;
     static HashMap<String, String> codes;
 
     private static void createQueue(String s) {
         text = s;
+        encoded = "";
         queue = new Queue();
         codes = new HashMap<>();
         int[] count = new int[256];
@@ -25,37 +28,66 @@ public class Huffman {
             }
         }
     }
+
     private static void find_char_code(Node node, String c) {
         if (node == null)
             return;
         // string with 1 length means we are on leaf node
         if (node.character.length() == 1) {
-            codes.put(node.character, c);
+            if (isEncoding)
+                codes.put(node.character, c);
+            else
+                codes.put(c, node.character);
             return;
         }
         find_char_code(node.left, c + "0");
         find_char_code(node.right, c + "1");
     }
 
-    static String encoding(String s) {
-        // create a sorted queue of tree
-        createQueue(s);
+    static private void createTree() {
         while (queue.lastIndex > 1) {
             Tree p = queue.dequeue();
             Tree q = queue.dequeue();
             queue.enqueue(Tree.concatTree(p, q));
         }
         huffmanTree = queue.dequeue();
+    }
+
+    static String encoding(String s) {
+        isEncoding = true;
+        // create a sorted queue of tree
+        createQueue(s);
+
+        //create huffman tree
+        createTree();
 
         //add each char and its special code in hashmap
         find_char_code(huffmanTree.root, "");
 
-        String result = "";
         //add code char by char to result
         for (int i = 0; i < text.length(); i++) {
-            result += codes.get(text.charAt(i) + "");
+            encoded += codes.get(text.charAt(i) + "");
         }
-        return result;
+        return encoded;
+    }
+
+    static String decoding(Queue newQueue, String encodedText) {
+        isEncoding = false;
+        codes = new HashMap<>();
+        queue = newQueue;
+        createTree();
+        find_char_code(huffmanTree.root, "");
+
+        int pos = 0;
+        String res = "";
+        for (int i = 0; i <= encodedText.length(); i++) {
+            String temp = encodedText.substring(pos, i);
+            if (codes.containsKey(temp)) {
+                res += codes.get(temp);
+                pos = i;
+            }
+        }
+        return res;
     }
 
 }
